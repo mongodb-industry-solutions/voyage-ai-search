aa7427b6dd1f32aab418b754861b30d2d6fc40f3
Benjamin Lorenz <ben@pocket.services>
2026-01-26 13:16:03 +0100

    start script path changes

--- bin/start.sh
+++ bin/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 . /usr/local/share/rt/bin/activate
-cd /usr/local/share/telco.chat || exit 1
+cd /usr/local/share/voyage-ai-search || exit 1
 
 /usr/pkg/sbin/daemonize \
   -u bjjl \
-  -c /usr/local/share/telco.chat \
-  -o /tmp/telco.log -e /tmp/telco.log \
-  -p /var/run/telco.pid -l /var/run/telco.lock \
+  -c /usr/local/share/voyage-ai-search \
+  -o /tmp/voyage.log -e /tmp/voyage.log \
+  -p /var/run/voyage.pid -l /var/run/voyage.lock \
   /usr/local/share/rt/bin/python app.py
